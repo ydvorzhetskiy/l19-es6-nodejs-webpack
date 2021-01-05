a03262fe35a5b5c20f37c968fc56494379dc7ed4
ydvorzhetskiy <dvorzhetskij@mail.ru>
2021-01-05 22:54:07 +0300

    Small fix

--- src/alert.js
+++ src/alert.js
@@ -1,6 +1,4 @@
 
-// export const myAlert = () => alert('!!!!');
-
 export const myAlert = () => fetch('/api')
-    .then(response => response)
-    .then(response => alert(response.body));
+     .then(response => response)
+     .then(response => alert(response.body));
